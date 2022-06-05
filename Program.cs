96c8254032b5a3484c0a757c3f4f7acd5104046f
KessyRamos <kessyellenramos@gmail.com>
2022-06-05 09:53:20 -0300

    PrimeiroCommit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double n1 = 9;
+double n2 = 10;
+double n3 = 8;
+double n4 = 6;
+double media;
+
+Console.WriteLine("Calcular media da nota do aluno!!!");
+
+media = (n1 + n2 + n3 + n4) / 4;
+Console.WriteLine("");
+
+Console.WriteLine($"A média do aluno é {media}");
+
+Console.WriteLine("");
+if (media > 7 ){
+    Console.WriteLine("O aluno foi APROVADO!!");
+} else{
+    Console.WriteLine("O aluno foi REPROVADO!");
+}
